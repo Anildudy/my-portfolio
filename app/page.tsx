55d6730008653d640d31bd79dfbcbf7a743ed4d2
Anildudy <anildudy1@gmail.com>
2025-11-30 21:53:08 +0530

    added contact, footer and work component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import Navbar from "./components/Navbar.jsx"
 import Header from "./components/Header.jsx"
 import About from "./components/About.jsx"
 import Services from "./components/Services.jsx"
+import Work from "./components/Work.jsx"
+import Contact from "./components/Contact.jsx"
+import Footer from "./components/Footer.jsx"
+
 export default function Home() {
   return (
     <>
@@ -10,6 +14,9 @@ export default function Home() {
       <Header />
       <About />
       <Services />
+      <Work />
+      <Contact />
+      <Footer />
     </>
   );
 }
