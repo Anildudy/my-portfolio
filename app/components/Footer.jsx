83dfc9e3428b9d08c7c6223ea06d1ddee4d9f536
Anildudy <anildudy1@gmail.com>
2025-12-06 22:46:53 +0530

    used preetier ot format code

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Image from "next/image";
-import { assets } from "@/assets/assets"
+import { assets } from "@/assets/assets";
 
-const Footer = ({isDarkMode}) => {
+const Footer = ({ isDarkMode }) => {
   return (
     <div className="mt-20">
       <div className="text-center">
-        <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt="" className="w-36 mx-auto mb-2" />
+        <Image
+          src={isDarkMode ? assets.logo_dark : assets.logo}
+          alt=""
+          className="w-36 mx-auto mb-2"
+        />
 
         <div className="w-max flex items-center gap-2 mx-auto">
-          <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt="" className="w-6" />
+          <Image
+            src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon}
+            alt=""
+            className="w-6"
+          />
           anildudy1@gmail.com
         </div>
       </div>
@@ -17,13 +25,28 @@ const Footer = ({isDarkMode}) => {
       <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
         <p> @ 2025 Anil Dudy. All rights reserved </p>
         <ul className="flex items-center gap-10 justify-center  mt-4 sm:mt-0">
-          <li> <a target="_blank" href="https://github.com/anildudy" >Github</a></li>
-          <li> <a target="_blank" href="https://linkedln.com/anildudy" >Linkedln</a></li>
-          <li> <a target="_blank" href="https://codeforces.com/anildudy" >Codeforces</a></li>
+          <li>
+            {" "}
+            <a target="_blank" href="https://github.com/anildudy">
+              Github
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a target="_blank" href="https://linkedln.com/anildudy">
+              Linkedln
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a target="_blank" href="https://codeforces.com/anildudy">
+              Codeforces
+            </a>
+          </li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -5,49 +5,71 @@ import { motion } from "motion/react";
 
 const Services = () => {
   return (
-    <motion.div id="Services" className="w-full px-[12%] py-10 scroll-mt-20"
-      initial={{opacity:0}}
-      whileInView={{opacity:1}}
-      transition={{duration:1}}
+    <motion.div
+      id="Services"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
     >
-      <motion.h4 className="text-center mb-2 text-lg font-Ovo"
-      initial={{y:-20, opacity:0}}
-      whileInView={{y:0, opacity:1}}
-      transition={{delay:0.3, duration:1}}
-      > What I Offer </motion.h4>
-      
-      <motion.h2 className="text-center text-5xl font-Ovo"
-      initial={{y:-20, opacity:0}}
-      whileInView={{y:0, opacity:1}}
-      transition={{delay:0.5, duration:0.5}}
-      > My Services </motion.h2>
+      <motion.h4
+        className="text-center mb-2 text-lg font-Ovo"
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 1 }}
+      >
+        {" "}
+        What I Offer{" "}
+      </motion.h4>
 
-      <motion.p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"
-      initial={{y:-20, opacity:0}}
-      whileInView={{y:0, opacity:1}}
-      transition={{delay:0.7, duration:0.5}}
+      <motion.h2
+        className="text-center text-5xl font-Ovo"
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
       >
-        I am a fullstack engineer with 3 years of experience from companies like barclays and finfactor
+        {" "}
+        My Services{" "}
+      </motion.h2>
+
+      <motion.p
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.7, duration: 0.5 }}
+      >
+        I am a fullstack engineer with 3 years of experience from companies like
+        barclays and finfactor
       </motion.p>
-      
-      <motion.div className="grid grid-template-auto gap-6 my-10"
-      initial={{y:-20, opacity:0}}
-      whileInView={{y:0, opacity:1}}
-      transition={{delay:0.9, duration:0.6}}
+
+      <motion.div
+        className="grid grid-template-auto gap-6 my-10"
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.9, duration: 0.6 }}
       >
-        {serviceData.map(({icon, title, description, link}, index) => (
-          <motion.div whileHover={{scale:1.05}} key={index} className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lighthover hover:-translate-y-1 duration-500 dark:hover:bg-darkhover dark:hover:shadow-white">
+        {serviceData.map(({ icon, title, description, link }, index) => (
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            key={index}
+            className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lighthover hover:-translate-y-1 duration-500 dark:hover:bg-darkhover dark:hover:shadow-white"
+          >
             <Image src={icon} alt="" className="w-10" />
-            <h3 className="text-lg my-4 text-gray-700 dark:text-white">{title}</h3>
-            <p className="text-sm text-gray-600 leading-5 dark:text-white/80">{description}</p>
+            <h3 className="text-lg my-4 text-gray-700 dark:text-white">
+              {title}
+            </h3>
+            <p className="text-sm text-gray-600 leading-5 dark:text-white/80">
+              {description}
+            </p>
             <a href={link} className="flex items-center gap-2 text-sm mt-5">
-                Read More <Image src={assets.right_arrow} alt='' className="w-4" />
-                </a>
+              Read More{" "}
+              <Image src={assets.right_arrow} alt="" className="w-4" />
+            </a>
           </motion.div>
         ))}
       </motion.div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
